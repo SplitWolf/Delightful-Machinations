e22554edb0e06e9b9cbc78a37822e19abbe29ee5
Coco <163203994+Coco-chii@users.noreply.github.com>
2024-07-17 02:11:53 -0400

    Added Mekanism crushing for Silent Gear
    Added ingot to dust recipes for Silent Gear ingots.

--- kubejs/server_scripts/src/mekanism.js
+++ kubejs/server_scripts/src/mekanism.js
@@ -1,3 +1,36 @@
 ServerEvents.recipes(event => {
     event.remove({ id: 'mekanism:rails' })
+
+    function mekanismCrushing(input, output, outputAmount) {
+        event.custom({
+          'type': 'mekanism:crushing',
+
+          'input': {
+            'ingredient': {
+              'tag': input
+            }
+          },
+
+          'output': {
+            'item': output,
+            'count': outputAmount
+          },
+
+          'conditions': [{
+              'type': 'forge:mod_loaded',
+              'modid': 'mekanism'
+          }]
+
+        })
+    }
+    
+    // Silent Gear crushing recipes
+
+    mekanismCrushing("forge:ingots/blaze_gold", "silentgear:blaze_gold_dust", 1)
+    mekanismCrushing("forge:ingots/crimson_iron", "silentgear:crimson_iron_dust", 1)
+    mekanismCrushing("forge:ingots/crimson_steel", "silentgear:crimson_steel_dust", 1)
+    mekanismCrushing("forge:ingots/azure_silver", "silentgear:azure_silver_dust", 1)
+    mekanismCrushing("forge:ingots/azure_electrum", "silentgear:azure_electrum_dust", 1)
+    mekanismCrushing("forge:ingots/tyrian_steel", "silentgear:tyrian_steel_dust", 1)
+
   })
